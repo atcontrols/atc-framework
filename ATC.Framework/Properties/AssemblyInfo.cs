ba6f31627701854394e6f599bebe6836d2f1cdc8
Fraser McLean <dev@frasermclean.com>
2021-06-02 11:45:47 +1000

    Updated project properties

--- ATC.Framework/Properties/AssemblyInfo.cs
+++ ATC.Framework/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ATC Framework")]
+[assembly: AssemblyDescription("Crestron C# development framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Framework")]
+[assembly: AssemblyCompany("AT Controls")]
+[assembly: AssemblyProduct("ATC Framework")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.2.0")]
+[assembly: AssemblyFileVersion("0.6.2.0")]
